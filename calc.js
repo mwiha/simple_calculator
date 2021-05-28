47fa0d3957eb7e62dc46d49e7473200a2da0d539
mwiha <alicemwihaki99@gmail.com>
2021-05-28 16:49:41 +0300

    created output functionality

--- calc.js
+++ calc.js
@@ -4,3 +4,14 @@ function getHistory(){
 function printHistory(num){
 	document.getElementById("history-value").innerText=num;
 }
+function getOutput(){
+	return document.getElementById("output-value").innerText;
+}
+function printOutput(num){
+	if(num==""){
+		document.getElementById("output-value").innerText=num;
+	}
+	else{
+		document.getElementById("output-value").innerText=getFormattedNumber(num);
+	}	
+}
